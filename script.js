d81575aa0cf59bbf8d9fc5225754ef5377293cf3
Mikhail Omelko <omelkom@gmail.com>
2020-10-27 21:41:17 +1000

    first commit

---
+++ script.js
@@ -0,0 +1,18 @@
+let js = 'amazing';
+if (js === 'amazing') console.log('JSss is amazing');
+
+
+console.log(2+2);
+
+let country = 'Russia';
+let continent = 'Eurasia';
+let population = 146000000;
+
+console.log(country);
+console.log(continent);
+console.log(population);
+
+
+let var1 = true;
+
+console.log(typeof var1 );
